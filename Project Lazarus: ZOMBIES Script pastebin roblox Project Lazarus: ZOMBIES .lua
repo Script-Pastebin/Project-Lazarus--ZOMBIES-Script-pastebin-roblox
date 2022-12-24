e12b8b05f63f32a238dce067b4f0569765c2516a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:03 +0100

    commit zero

---
+++ Project Lazarus: ZOMBIES Script pastebin roblox Project Lazarus: ZOMBIES .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/AmdBs5Bc", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							This is a cool script.It has:Infinite ammo, ESP, Fly (C), Noclip (X) and more!
+						
+]]
